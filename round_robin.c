e58fbd551ae1163c7b23242c4b659e6a8caea496
user name <eduardasm@dcc.ufrj.br>
2023-11-14 23:25:57 -0300

    add printaLista & insereNaLista

--- round_robin.c
+++ round_robin.c
@@ -12,6 +12,7 @@ typedef struct Processo {
 typedef struct No {
     Processo processo;
     int tempoRestante;
+    int prioridade;
     struct No* proximo;
     struct No* anterior;
 } No;
@@ -24,26 +25,55 @@ struct No* criaNo(Processo processo) {
     return novoNo;
 }
 
-void insereOrdenado(struct No* head, Processo processo) {
+void insereNaLista(struct No** head, Processo processo) {
     struct No* novoNo = criaNo(processo);
-    struct No* temp = head;
-    if(head==NULL){
-        head->proximo = novoNo;
-    }
-    else{
-        while(novoNo->processo.prioridade > temp->proximo->processo.prioridade && temp->proximo != NULL){
+    if (*head == NULL) {
+        *head = novoNo;
+    } 
+    else {
+        struct No* temp = *head;
+        while (temp->proximo != NULL) {
             temp = temp->proximo;
         }
-        
+        temp->proximo = novoNo;
+        novoNo->anterior = temp;
+
     }
+}
 
 
+void organizaPorPrioridade(No** head) {
+   // TO DO!!
+}
+
+void printaLista(struct No* head) {
+    struct No* temp = head;
+    while (temp != NULL) {
+        printf("PID: %d, Prioridade: %d, Tempo de Serviço: %d\n", temp->processo.pid, temp->processo.prioridade, temp->processo.ts);
+        temp = temp->proximo;
+    }
 }
 
 
 int main (){
-    // Inicializa a lista como vazia
     struct No* head = NULL;
+   
+    // Adiciona 3 processos à lista
+    Processo processo1 = {1, 3, 10};
+    Processo processo2 = {2, 1, 8};
+    Processo processo3 = {3, 2, 12};
+
+    insereNaLista(&head, processo1);
+    insereNaLista(&head, processo2);
+    insereNaLista(&head, processo3);
+
+    printf("Lista antes de organizar por prioridade:\n");
+    imprimeLista(head);
+
+    organizaPorPrioridade(head);
+
+    printf("\nLista depois de organizar por prioridade:\n");
+    printaLista(head);
 
 
 return 0;
